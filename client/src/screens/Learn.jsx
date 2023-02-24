5e3c6814bb1c9b7c8069fb995eed5e7509d53624
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-02-25 04:14:25 +0530

    Delete client directory

--- client/src/screens/Learn.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LightMode from "../components/LightMode";
-
-const Learn = () => {
-  return <div className="Learn full-height">Learn</div>;
-};
-
-export default Learn;
